f092cb17e7de3576ca039656090366d4f1a04623
André Silva <andre06x@gmail.com>
2023-03-07 18:49:34 -0300

    registro rotas provisorio

--- src/becape-components/Pages/Operador/index.js
+++ src/becape-components/Pages/Operador/index.js
@@ -1,174 +1,216 @@
 import React from "react";
 import { MdCall } from "react-icons/md";
+import { registrarRotas } from "../../../routes";
 import { TabelaChamados } from "../../Components/TabelaChamados";
 import { Historico } from "../Historico";
 
 const Operador = () => {
-  return (
-    <div className="row">
-      <div className="col-12 col-md-9">
-        <div className="bg-light rounded p-3">
-          <div className="d-flex align-items-center justify-content-between">
-            <h3>Ana Pereira da Costa</h3>
-            <h4>CPF: 168.555.807-00</h4>
-          </div>
+    return (
+        <div className="row">
+            <div className="col-12 col-md-9">
+                <div className="bg-light rounded p-3">
+                    <div className="d-flex align-items-center justify-content-between">
+                        <h3>Ana Pereira da Costa</h3>
+                        <h4>CPF: 168.555.807-00</h4>
+                    </div>
 
-          <div className="d-flex align-items-center justify-content-between mb-5">
-            <div>
-              <span className="mr-2">42 anos</span>
-              <span>Telefone: 4002-8922</span>
+                    <div className="d-flex align-items-center justify-content-between mb-5">
+                        <div>
+                            <span className="mr-2">42 anos</span>
+                            <span>Telefone: 4002-8922</span>
+                        </div>
+                        <span>Endereço: Estrada velha N° 42</span>
+                    </div>
+
+                    <div>
+                        <h5 className="mb-3">Produtos Recomendados</h5>
+                        <div className="row">
+                            <div className="col-3">
+                                <div className="d-flex flex-column">
+                                    <img
+                                        width="100"
+                                        src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
+                                        alt=""
+                                    />
+                                    <span>Loratamed</span>
+                                    <span>R$ 3,90</span>
+                                </div>
+                            </div>
+
+                            <div className="col-3">
+                                <div className="d-flex flex-column">
+                                    <img
+                                        width="100"
+                                        src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
+                                        alt=""
+                                    />
+                                    <span>Loratamed</span>
+                                    <span>R$ 3,90</span>
+                                </div>
+                            </div>
+
+                            <div className="col-3">
+                                <div className="d-flex flex-column">
+                                    <img
+                                        width="100"
+                                        src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
+                                        alt=""
+                                    />
+                                    <span>Loratamed</span>
+                                    <span>R$ 3,90</span>
+                                </div>
+                            </div>
+
+                            <div className="col-3">
+                                <div className="d-flex flex-column">
+                                    <img
+                                        width="100"
+                                        src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
+                                        alt=""
+                                    />
+                                    <span>Loratamed</span>
+                                    <span>R$ 3,90</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <button onClick={() => registrarRotas()} />
+                <Historico />
             </div>
-            <span>Endereço: Estrada velha N° 42</span>
-          </div>
+            <div className="col-12 col-md-3">
+                <div className="p-3 bg-light rounded d-flex flex-column">
+                    <h5 className="mb-3">Chamadas em fila - 5</h5>
 
-          <div>
-            <h5 className="mb-3">Produtos Recomendados</h5>
-            <div className="row">
-              <div className="col-3">
-                <div className="d-flex flex-column">
-                  <img
-                    width="100"
-                    src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
-                    alt=""
-                  />
-                  <span>Loratamed</span>
-                  <span>R$ 3,90</span>
-                </div>
-              </div>
+                    <div className="d-flex align-items-center">
+                        <input type="checkbox" name="" id="" />
+                        <label class="p-0 m-0 mx-1" htmlFor="">
+                            Atender Automaticamente
+                        </label>
+                    </div>
 
-              <div className="col-3">
-                <div className="d-flex flex-column">
-                  <img
-                    width="100"
-                    src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
-                    alt=""
-                  />
-                  <span>Loratamed</span>
-                  <span>R$ 3,90</span>
-                </div>
-              </div>
+                    <div>
+                        <div
+                            className="p-2 px-4 my-3 rounded position-relative"
+                            style={{ backgroundColor: "#edf2f9" }}
+                        >
+                            <div className="d-flex mb-2 align-items-center justify-content-between">
+                                <span>Ana</span>
+                                <span>16:00/00:35</span>
+                            </div>
+                            <span className="d-block text-right">
+                                (21)9876-9876
+                            </span>
+                            <div
+                                className="position-absolute"
+                                style={{ left: -10, top: 25 }}
+                            >
+                                <button
+                                    className="bg-success d-flex justify-content-center align-items-center"
+                                    style={{
+                                        width: 35,
+                                        height: 35,
+                                        borderRadius: 17.5,
+                                        border: 0
+                                    }}
+                                >
+                                    <MdCall size={20} color="#fff" />
+                                </button>
+                            </div>
+                        </div>
 
-              <div className="col-3">
-                <div className="d-flex flex-column">
-                  <img
-                    width="100"
-                    src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
-                    alt=""
-                  />
-                  <span>Loratamed</span>
-                  <span>R$ 3,90</span>
-                </div>
-              </div>
+                        <div
+                            className="p-2 px-4 my-3 rounded position-relative"
+                            style={{ backgroundColor: "#edf2f9" }}
+                        >
+                            <div className="d-flex mb-2 align-items-center justify-content-between">
+                                <span>Ana</span>
+                                <span>16:00/00:35</span>
+                            </div>
+                            <span className="d-block text-right">
+                                (21)9876-9876
+                            </span>
+                            <div
+                                className="position-absolute"
+                                style={{ left: -10, top: 25 }}
+                            >
+                                <button
+                                    className="bg-success d-flex justify-content-center align-items-center"
+                                    style={{
+                                        width: 35,
+                                        height: 35,
+                                        borderRadius: 17.5,
+                                        border: 0
+                                    }}
+                                >
+                                    <MdCall size={20} color="#fff" />
+                                </button>
+                            </div>
+                        </div>
 
-              <div className="col-3">
-                <div className="d-flex flex-column">
-                  <img
-                    width="100"
-                    src="https://imgs.ponto.com.br/1523906528/1xg.jpg"
-                    alt=""
-                  />
-                  <span>Loratamed</span>
-                  <span>R$ 3,90</span>
+                        <div
+                            className="p-2 px-4 my-3 rounded position-relative"
+                            style={{ backgroundColor: "#edf2f9" }}
+                        >
+                            <div className="d-flex mb-2 align-items-center justify-content-between">
+                                <span>Ana</span>
+                                <span>16:00/00:35</span>
+                            </div>
+                            <span className="d-block text-right">
+                                (21)9876-9876
+                            </span>
+                            <div
+                                className="position-absolute"
+                                style={{ left: -10, top: 25 }}
+                            >
+                                <button
+                                    className="bg-success d-flex justify-content-center align-items-center"
+                                    style={{
+                                        width: 35,
+                                        height: 35,
+                                        borderRadius: 17.5,
+                                        border: 0
+                                    }}
+                                >
+                                    <MdCall size={20} color="#fff" />
+                                </button>
+                            </div>
+                        </div>
+
+                        <div
+                            className="p-2 px-4 my-3 rounded position-relative"
+                            style={{ backgroundColor: "#edf2f9" }}
+                        >
+                            <div className="d-flex mb-2 align-items-center justify-content-between">
+                                <span>Ana</span>
+                                <span>16:00/00:35</span>
+                            </div>
+                            <span className="d-block text-right">
+                                (21)9876-9876
+                            </span>
+                            <div
+                                className="position-absolute"
+                                style={{ left: -10, top: 25 }}
+                            >
+                                <button
+                                    className="bg-success d-flex justify-content-center align-items-center"
+                                    style={{
+                                        width: 35,
+                                        height: 35,
+                                        borderRadius: 17.5,
+                                        border: 0
+                                    }}
+                                >
+                                    <MdCall size={20} color="#fff" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-
-        <Historico />
-      </div>
-      <div className="col-12 col-md-3">
-        <div className="p-3 bg-light rounded d-flex flex-column">
-          <h5 className="mb-3">Chamadas em fila - 5</h5>
-
-          <div className="d-flex align-items-center">
-            <input type="checkbox" name="" id="" />
-            <label class="p-0 m-0 mx-1" htmlFor="">
-              Atender Automaticamente
-            </label>
-          </div>
-
-          <div>
-            <div
-              className="p-2 px-4 my-3 rounded position-relative"
-              style={{ backgroundColor: "#edf2f9" }}
-            >
-              <div className="d-flex mb-2 align-items-center justify-content-between">
-                <span>Ana</span>
-                <span>16:00/00:35</span>
-              </div>
-              <span className="d-block text-right">(21)9876-9876</span>
-              <div className="position-absolute" style={{ left: -10, top: 25 }}>
-                <button
-                  className="bg-success d-flex justify-content-center align-items-center"
-                  style={{ width: 35, height: 35, borderRadius: 17.5, border: 0 }}
-                >
-                  <MdCall size={20} color="#fff" />
-                </button>
-              </div>
-            </div>
-
-            <div
-              className="p-2 px-4 my-3 rounded position-relative"
-              style={{ backgroundColor: "#edf2f9" }}
-            >
-              <div className="d-flex mb-2 align-items-center justify-content-between">
-                <span>Ana</span>
-                <span>16:00/00:35</span>
-              </div>
-              <span className="d-block text-right">(21)9876-9876</span>
-              <div className="position-absolute" style={{ left: -10, top: 25 }}>
-                <button
-                  className="bg-success d-flex justify-content-center align-items-center"
-                  style={{ width: 35, height: 35, borderRadius: 17.5, border: 0 }}
-                >
-                  <MdCall size={20} color="#fff" />
-                </button>
-              </div>
-            </div>
-
-            <div
-              className="p-2 px-4 my-3 rounded position-relative"
-              style={{ backgroundColor: "#edf2f9" }}
-            >
-              <div className="d-flex mb-2 align-items-center justify-content-between">
-                <span>Ana</span>
-                <span>16:00/00:35</span>
-              </div>
-              <span className="d-block text-right">(21)9876-9876</span>
-              <div className="position-absolute" style={{ left: -10, top: 25 }}>
-                <button
-                  className="bg-success d-flex justify-content-center align-items-center"
-                  style={{ width: 35, height: 35, borderRadius: 17.5, border: 0 }}
-                >
-                  <MdCall size={20} color="#fff" />
-                </button>
-              </div>
-            </div>
-
-            <div
-              className="p-2 px-4 my-3 rounded position-relative"
-              style={{ backgroundColor: "#edf2f9" }}
-            >
-              <div className="d-flex mb-2 align-items-center justify-content-between">
-                <span>Ana</span>
-                <span>16:00/00:35</span>
-              </div>
-              <span className="d-block text-right">(21)9876-9876</span>
-              <div className="position-absolute" style={{ left: -10, top: 25 }}>
-                <button
-                  className="bg-success d-flex justify-content-center align-items-center"
-                  style={{ width: 35, height: 35, borderRadius: 17.5, border: 0 }}
-                >
-                  <MdCall size={20} color="#fff" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export { Operador };
--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,84 +2,102 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Button, Form, Row, Col, FormGroup, Input, CustomInput, Label } from "reactstrap";
+import {
+    Button,
+    Form,
+    Row,
+    Col,
+    FormGroup,
+    Input,
+    CustomInput,
+    Label
+} from "reactstrap";
 import Divider from "../common/Divider";
 import SocialAuthButtons from "./SocialAuthButtons";
 import withRedirect from "../../hoc/withRedirect";
 import Cookies from "js-cookie";
+import { registrarRotas } from "../../routes";
 
 const LoginForm = ({ setRedirect, hasLabel, layout }) => {
-  // State
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [remember, setRemember] = useState(true);
-  const [isDisabled, setIsDisabled] = useState(true);
+    // State
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [remember, setRemember] = useState(true);
+    const [isDisabled, setIsDisabled] = useState(true);
 
-  // Handler
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    // Handler
+    const handleSubmit = e => {
+        e.preventDefault();
 
-    if (email === "operador@gmail.com") {
-      Cookies.set("perfil", "operador", { expires: 1 });
-      Cookies.set("acesso", "true", { expires: 1 });
+        if (email === "operador@gmail.com") {
+            Cookies.set("perfil", "operador", { expires: 1 });
+            Cookies.set("acesso", "true", { expires: 1 });
 
-      toast.success(`Logado com ${email}`);
-      setRedirect(true);
-    }
-    if (email === "adm@gmail.com") {
-      Cookies.set("acesso", true, { expires: 1 });
-      Cookies.set("perfil", "adm", { expires: 1 });
+            toast.success(`Logado com ${email}`);
+            setRedirect(true);
+            registrarRotas();
+        }
+        if (email === "adm@gmail.com") {
+            Cookies.set("acesso", true, { expires: 1 });
+            Cookies.set("perfil", "adm", { expires: 1 });
 
-      toast.success(`Logado com ${email}`);
-      setRedirect(true);
-    }
-    toast.success(`Email inválido`);
-  };
+            toast.success(`Logado com ${email}`);
+            setRedirect(true);
+            registrarRotas();
+        }
 
-  useEffect(() => {
-    setIsDisabled(!email || !password);
-  }, [email, password]);
+        // toast.success(`Email inválido`);
+    };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        {hasLabel && <Label>Email address</Label>}
-        <Input
-          placeholder={!hasLabel ? "Email" : ""}
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          type="email"
-        />
-      </FormGroup>
-      <FormGroup>
-        {hasLabel && <Label>Password</Label>}
-        <Input
-          placeholder={!hasLabel ? "Senhaa" : ""}
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          type="password"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Button color="primary" block className="mt-3" disabled={isDisabled}>
-          Login
-        </Button>
-      </FormGroup>
-      {/* <Divider className="mt-4">or log in with</Divider> */}
-      {/* <SocialAuthButtons /> */}
-    </Form>
-  );
+    useEffect(() => {
+        setIsDisabled(!email || !password);
+    }, [email, password]);
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                {hasLabel && <Label>Email address</Label>}
+                <Input
+                    placeholder={!hasLabel ? "Email" : ""}
+                    value={email}
+                    onChange={({ target }) => setEmail(target.value)}
+                    type="email"
+                />
+            </FormGroup>
+            <FormGroup>
+                {hasLabel && <Label>Password</Label>}
+                <Input
+                    placeholder={!hasLabel ? "Senhaa" : ""}
+                    value={password}
+                    onChange={({ target }) => setPassword(target.value)}
+                    type="password"
+                />
+            </FormGroup>
+            <FormGroup>
+                <Button
+                    color="primary"
+                    block
+                    className="mt-3"
+                    disabled={isDisabled}
+                >
+                    Login
+                </Button>
+            </FormGroup>
+            {/* <Divider className="mt-4">or log in with</Divider> */}
+            {/* <SocialAuthButtons /> */}
+        </Form>
+    );
 };
 
 LoginForm.propTypes = {
-  setRedirect: PropTypes.func.isRequired,
-  layout: PropTypes.string,
-  hasLabel: PropTypes.bool,
+    setRedirect: PropTypes.func.isRequired,
+    layout: PropTypes.string,
+    hasLabel: PropTypes.bool
 };
 
 LoginForm.defaultProps = {
-  layout: "basic",
-  hasLabel: false,
+    layout: "basic",
+    hasLabel: false
 };
 
 export default withRedirect(LoginForm);
