03f1caf976c3378367c8ca88c6ca940b6b471ee4
André Silva <andre06x@gmail.com>
2023-03-18 09:08:37 -0300

    estruturando component formcliente

--- src/becape-components/Components/CadastrarCliente/index.js
+++ src/becape-components/Components/CadastrarCliente/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-const FormCadastrarCliente = ({ telefone }) => {
+export const FormCliente = ({ telefone }) => {
   return (
     <div className="d-flex flex-column">
-      <h5 className="mb-3">Cadastar Cliente</h5>
       <div className="row mb-4">
         <div className="col-7">
           <div className="d-flex">
@@ -41,10 +40,43 @@ const FormCadastrarCliente = ({ telefone }) => {
       </div>
 
       <div className="d-flex mb-4">
+        <span className="mr-3">CEP:</span>
+        <input className="form-control" type="text" />
+      </div>
+
+      <div className="d-flex mb-4">
         <span className="mr-3">Endereço:</span>
         <input className="form-control" type="text" />
       </div>
+
+      <div className="d-flex mb-4">
+        <span className="mr-3">Bairro:</span>
+        <input className="form-control" type="text" />
+      </div>
+
+      <div className="d-flex mb-4">
+        <span className="mr-3">Estado:</span>
+        <select className="form-control" type="text">
+          <option value="">RJ</option>
+        </select>
+      </div>
+
+      <div className="d-flex mb-4">
+        <span className="mr-3">Cidade:</span>
+        <input className="form-control" type="text" />
+      </div>
     </div>
+  );
+};
+const FormCadastrarCliente = ({ telefone }) => {
+  return (
+    <>
+      <h5 className="mb-3">Cadastar Cliente</h5>
+      <FormCliente telefone={telefone} />
+      <button className="btn btn-success" style={{ width: 120 }}>
+        Cadastar
+      </button>
+    </>
   );
 };
 
