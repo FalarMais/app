2fad9f60291546c3749816e921f8aed5a56cfcac
André Silva <andre06x@gmail.com>
2023-03-21 17:28:40 -0300

    21/03

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef16cbdf0476d762090cf3edaaba4449.js"
+  "/precache-manifest.305cd8d6768b288cedeb25f98fc5a96c.js"
 );
 
 self.addEventListener('message', (event) => {
