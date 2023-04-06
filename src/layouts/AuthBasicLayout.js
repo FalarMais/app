1616a747010a6b8b1598f2a2dffc361b007d07b5
André Silva <andre06x@gmail.com>
2023-04-06 17:14:49 -0300

    add attr width_logo

--- src/layouts/AuthBasicLayout.js
+++ src/layouts/AuthBasicLayout.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Card, CardBody, Col, Row } from 'reactstrap';
-import Logo from '../components/navbar/Logo';
-import Section from '../components/common/Section';
-import AuthBasicRoutes from '../components/auth/basic/AuthBasicRoutes';
+import React from "react";
+import { Card, CardBody, Col, Row } from "reactstrap";
+import Logo from "../components/navbar/Logo";
+import Section from "../components/common/Section";
+import AuthBasicRoutes from "../components/auth/basic/AuthBasicRoutes";
 
 const AuthBasicLayout = () => (
   <Section className="py-0">
     <Row className="flex-center min-vh-100 py-6">
       <Col sm={10} md={8} lg={6} xl={5} className="col-xxl-4">
-        <Logo />
+        <Logo width_logo={200} />
         <Card>
           <CardBody className="fs--1 font-weight-normal p-5">
             <AuthBasicRoutes />
