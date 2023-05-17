9c91799b56280d969226ce4e08c92c7fa530b64d
André Silva <andre06x@gmail.com>
2023-05-17 17:06:48 -0300

    alterando form ramal

--- src/becape-components/Components/FormRamal/index.js
+++ src/becape-components/Components/FormRamal/index.js
@@ -12,37 +12,14 @@ const FormRamal = ({ tipo }) => {
     <div className="my-4">
       <div className="" />
       <div className="row">
-        {tipo === "add" ? (
-          <div className="col-4 ">
-            <div>
-              <label htmlFor="">Ramal</label>
-              <input type="text" className="form-control" />
-            </div>
-          </div>
-        ) : null}
-
-        <div className="col-4">
+        <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Nome Utilizador</label>
             <input type="text" className="form-control" />
           </div>
         </div>
-        <div className="col-4">
-          <div>
-            <label htmlFor="">Realiza chamada</label>
-            <select name="" id="" className="form-control">
-              <option value="">SIM</option>
-              <option value="">NAO</option>
-            </select>
-          </div>
-        </div>
-        <div className="col-4">
-          <div>
-            <label htmlFor="">Numero do ramal</label>
-            <input type="text" className="form-control" />
-          </div>
-        </div>
-        <div className="col-4">
+
+        <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Tipo do Ramal</label>
             <select
@@ -60,7 +37,7 @@ const FormRamal = ({ tipo }) => {
         </div>
 
         {form.tipo === "cisco" ? (
-          <div className="col-4">
+          <div className="col-3 mb-4">
             <div>
               <label htmlFor="">MAC</label>
               <input type="text" className="form-control" />
@@ -68,24 +45,105 @@ const FormRamal = ({ tipo }) => {
           </div>
         ) : null}
 
-        <div className="col-4">
+        <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Modelo</label>
             <input type="text" className="form-control" />
           </div>
         </div>
 
-        <div className="col-4">
+        <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Senha</label>
             <input type="text" className="form-control" />
           </div>
         </div>
 
-        <div className="col-4">
+        <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Call-limit</label>
             <input type="text" className="form-control" />
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">Call Group</label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">Pickup Group</label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">hash</label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">Ligação Externa</label>
+            <div className="d-flex ">
+              <div className="d-flex align-items-center mr-2">
+                <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
+                  Ligado
+                </label>
+                <input type="radio" name="ligacaoExterna" id="" />
+              </div>
+              <div className="d-flex align-items-center">
+                <label style={{ marginBottom: 0 }} className="mr-1">
+                  Desligado
+                </label>
+                <input type="radio" name="ligacaoExterna" id="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">Ligação Senha</label>
+            <div className="d-flex ">
+              <div className="d-flex align-items-center mr-2">
+                <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
+                  Ligado
+                </label>
+                <input type="radio" name="ligacaoSenha" id="" />
+              </div>
+              <div className="d-flex align-items-center">
+                <label style={{ marginBottom: 0 }} className="mr-1">
+                  Desligado
+                </label>
+                <input type="radio" name="ligacaoSenha" id="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-3 mb-4">
+          <div>
+            <label htmlFor="">aGrupa</label>
+            <div className="d-flex ">
+              <div className="d-flex align-items-center mr-2">
+                <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
+                  Ligado
+                </label>
+                <input type="radio" name="aGrupa" id="" />
+              </div>
+              <div className="d-flex align-items-center">
+                <label style={{ marginBottom: 0 }} className="mr-1">
+                  Desligado
+                </label>
+                <input type="radio" name="aGrupa" id="" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
