2dcd775d93acef1df1bad5fdf555c790896a4259
André Silva <andre06x@gmail.com>
2023-05-18 16:16:10 -0300

    refatorando componente formRamal com valores

--- src/becape-components/Components/FormRamal/index.js
+++ src/becape-components/Components/FormRamal/index.js
@@ -1,13 +1,47 @@
 import React, { useState } from "react";
 
-const FormRamal = ({ tipo }) => {
-  const [form, setForm] = useState({ tipo: "" });
+export const exemplo = {
+  nomeUtilizador: "teste",
+  tipo: "1",
+  senha: "string",
+  callLimit: 0,
+  ligacaoExterna: true,
+  ligacaoSenha: true,
+  aGrupa: true,
+  contaId: "e1d6605a-3f43-4a3b-8aa5-3820ba62953d",
+  pickupGroup: "string",
+  callGroup: "string",
+  mac: "string",
+  modelo: 0,
+  id: "1a6b18db-0f1b-4e12-b6d8-10aa65665e97"
+};
+
+const vazio = {
+  nomeUtilizador: "",
+  tipo: "",
+  senha: "",
+  callLimit: 0,
+  ligacaoExterna: false,
+  ligacaoSenha: false,
+  aGrupa: false,
+  contaId: "e1d6605a-3f43-4a3b-8aa5-3820ba62953d",
+  pickupGroup: "",
+  callGroup: "",
+  mac: "",
+  modelo: 0,
+  id: "1a6b18db-0f1b-4e12-b6d8-10aa65665e97"
+};
+const FormRamal = ({ tipo, data }) => {
+  const [form, setForm] = useState(data ? data : vazio);
 
   const changeForm = event => {
     const { name, value } = event.target;
     setForm({ ...form, [name]: value });
   };
 
+  const adicionarRamal = () => {
+    console.log(form);
+  };
   return (
     <div className="my-4">
       <div className="" />
@@ -15,7 +49,13 @@ const FormRamal = ({ tipo }) => {
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Nome Utilizador</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              onChange={changeForm}
+              value={form.nomeUtilizador}
+              name="nomeUtilizador"
+            />
           </div>
         </div>
 
@@ -26,21 +66,28 @@ const FormRamal = ({ tipo }) => {
               name="tipo"
               id=""
               className="form-control"
-              onChange={changeForm}
               value={form.tipo}
+              onChange={changeForm}
             >
-              <option value="padrao">Padrão</option>
-              <option value="softphone">Softphone</option>
-              <option value="cisco">Cisco</option>
+              <option value="">--SELECIONE--</option>
+              <option value="0">Padrão</option>
+              <option value="1">Softphone</option>
+              <option value="2">Cisco</option>
             </select>
           </div>
         </div>
 
-        {form.tipo === "cisco" ? (
+        {form.tipo === "2" ? (
           <div className="col-3 mb-4">
             <div>
               <label htmlFor="">MAC</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                name="mac"
+                onChange={changeForm}
+                value={form.mac}
+              />
             </div>
           </div>
         ) : null}
@@ -48,42 +95,65 @@ const FormRamal = ({ tipo }) => {
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Modelo</label>
-            <input type="text" className="form-control" />
+            <input
+              name="modelo"
+              onChange={changeForm}
+              value={form.modelo}
+              type="number"
+              className="form-control"
+            />
           </div>
         </div>
 
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Senha</label>
-            <input type="text" className="form-control" />
+            <input
+              name="senha"
+              onChange={changeForm}
+              value={form.senha}
+              type="text"
+              className="form-control"
+            />
           </div>
         </div>
 
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Call-limit</label>
-            <input type="text" className="form-control" />
+            <input
+              name="callLimit"
+              onChange={changeForm}
+              value={form.callLimit}
+              type="number"
+              className="form-control"
+            />
           </div>
         </div>
 
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Call Group</label>
-            <input type="text" className="form-control" />
+            <input
+              name="callGroup"
+              onChange={changeForm}
+              value={form.callGroup}
+              type="text"
+              className="form-control"
+            />
           </div>
         </div>
 
         <div className="col-3 mb-4">
           <div>
             <label htmlFor="">Pickup Group</label>
-            <input type="text" className="form-control" />
-          </div>
-        </div>
-
-        <div className="col-3 mb-4">
-          <div>
-            <label htmlFor="">hash</label>
-            <input type="text" className="form-control" />
+            <input
+              onChange={changeForm}
+              value={form.pickupGroup}
+              name="pickupGroup"
+              type="text"
+              className="form-control"
+            />
           </div>
         </div>
 
@@ -95,13 +165,24 @@ const FormRamal = ({ tipo }) => {
                 <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
                   Ligado
                 </label>
-                <input type="radio" name="ligacaoExterna" id="" />
+                <input
+                  type="radio"
+                  name="ligacaoExterna"
+                  checked={form.ligacaoExterna === true && true}
+                  onChange={() => setForm({ ...form, ligacaoExterna: true })}
+                />
               </div>
               <div className="d-flex align-items-center">
                 <label style={{ marginBottom: 0 }} className="mr-1">
                   Desligado
                 </label>
-                <input type="radio" name="ligacaoExterna" id="" />
+                <input
+                  name="ligacaoExterna"
+                  checked={form.ligacaoExterna === false && true}
+                  onChange={() => setForm({ ...form, ligacaoExterna: false })}
+                  type="radio"
+                  id=""
+                />
               </div>
             </div>
           </div>
@@ -115,13 +196,25 @@ const FormRamal = ({ tipo }) => {
                 <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
                   Ligado
                 </label>
-                <input type="radio" name="ligacaoSenha" id="" />
+                <input
+                  checked={form.ligacaoSenha === true && true}
+                  onChange={() => setForm({ ...form, ligacaoSenha: true })}
+                  name="ligacaoSenha"
+                  type="radio"
+                  id=""
+                />
               </div>
               <div className="d-flex align-items-center">
                 <label style={{ marginBottom: 0 }} className="mr-1">
                   Desligado
                 </label>
-                <input type="radio" name="ligacaoSenha" id="" />
+                <input
+                  checked={form.ligacaoSenha === false && true}
+                  onChange={() => setForm({ ...form, ligacaoSenha: false })}
+                  name="ligacaoSenha"
+                  type="radio"
+                  id=""
+                />
               </div>
             </div>
           </div>
@@ -135,18 +228,36 @@ const FormRamal = ({ tipo }) => {
                 <label style={{ marginBottom: 0 }} className="mr-1" htmlFor="">
                   Ligado
                 </label>
-                <input type="radio" name="aGrupa" id="" />
+                <input
+                  type="radio"
+                  checked={form.aGrupa === true && true}
+                  onChange={() => setForm({ ...form, aGrupa: true })}
+                  name="aGrupa"
+                  id=""
+                />
               </div>
               <div className="d-flex align-items-center">
                 <label style={{ marginBottom: 0 }} className="mr-1">
                   Desligado
                 </label>
-                <input type="radio" name="aGrupa" id="" />
+                <input
+                  type="radio"
+                  checked={form.aGrupa === false && true}
+                  onChange={() => setForm({ ...form, aGrupa: false })}
+                  name="aGrupa"
+                  id=""
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
+      <button
+        className="btn btn-outline-falar my-2"
+        onClick={() => adicionarRamal()}
+      >
+        {tipo}
+      </button>
     </div>
   );
 };
