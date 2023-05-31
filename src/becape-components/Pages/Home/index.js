e8d676fbef6e4ec36aa90c05b0b8088073a4d50a
André Silva <andre06x@gmail.com>
2023-05-31 17:30:05 -0300

    desacoplando grafico home

--- src/becape-components/Pages/Home/index.js
+++ src/becape-components/Pages/Home/index.js
@@ -1,85 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Cards } from "./Cards";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer
-} from "recharts";
+
 import { TabelaChamados } from "../../Components/TabelaChamados";
 import { Card } from "reactstrap";
 import { useEffect } from "react";
 import { useApiRequestEffect } from "../../hooks/useApiRequest";
 import Cookies from "js-cookie";
+import moment from "moment";
+import { Grafico } from "./Grafico";
 
+const hoje = moment().format("DD-MM-YYYY");
 const Home = () => {
   const contaId = Cookies.get("contaId");
-  const { response, setResponse, isLoading } = useApiRequestEffect(
-    `/chamada/conta/${contaId}`
+  const [datas, setDatas] = useState(hoje);
+  const { response, setResponse, isLoading, refetch } = useApiRequestEffect(
+    `chamada/periodo?contaid=${contaId}&dataInicial=${datas}`
   );
-  const data = [
-    {
-      name: "Dia 1",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400
-    },
-    {
-      name: "Dia 2",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210
-    },
-    {
-      name: "Dia 3",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290
-    },
-    {
-      name: "Dia 4",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000
-    },
-    {
-      name: "Dia 5",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181
-    },
-    {
-      name: "Dia 6",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500
-    },
-    {
-      name: "Dia 7",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100
-    }
-  ];
 
   useEffect(() => {
-    console.log(response);
-  }, [response]);
+    const momentt = moment().format("DD-MM-YYYY");
 
+    console.log(
+      momentt,
+      moment()
+        .subtract(1, "days")
+        .format("DD-MM-YYYY")
+    );
+  }, []);
+
+  const alterarData = event => {
+    const dia = event.target.value;
+
+    if (!dia) {
+      setDatas(hoje);
+    } else {
+      const novaData = moment()
+        .subtract(dia, "days")
+        .format("DD-MM-YYYY");
+      setDatas(novaData);
+    }
+  };
+
+  const procurarChamadas = () => {
+    refetch();
+  };
   return (
     <div>
       <div className="bg-light px-2 py-1 my-2">
         <span className="pr-2">Filtrar Tempo:</span>
-        <select name="" id="" className="mr-2">
+        <select
+          name=""
+          id=""
+          className="mr-2"
+          onChange={event => alterarData(event)}
+        >
           <option value="">Hoje</option>
-          <option value="">Ontem</option>
-          <option value="">Semana</option>
-          <option value="">Mes</option>
+          <option value="1">Ontem</option>
+          <option value="7">7 dias</option>
+          <option value="30">30 dias</option>
         </select>
-        <button className="btn btn-falar">Aplicar</button>
+        <button className="btn btn-falar" onClick={() => procurarChamadas()}>
+          Aplicar
+        </button>
       </div>
       <Cards dataChamadas={response} />
       {/* <ModalChamada /> */}
@@ -91,34 +73,7 @@ const Home = () => {
           isLoading={isLoading}
         />
       </Card>
-
-      <div style={{ width: "100%", height: 300 }} className="card col-12">
-        <h3 className="p-2 text-muted">Numero de chamadas por dia</h3>
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
+      <Grafico response={response} />
     </div>
   );
 };
