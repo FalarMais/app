d746d88d186d815986186f667110f7ab81ab9274
André Silva <andre06x@gmail.com>
2023-06-16 12:56:22 -0300

    desacoplando loading

--- src/becape-components/hooks/useApiRequest.js
+++ src/becape-components/hooks/useApiRequest.js
@@ -21,11 +21,12 @@ const useApiRequestEffect = (url, data = null) => {
 
       console.log(req);
       setResponse(req);
-      setIsLoading(false);
     } catch (err) {
       console.log(err);
       setError(err);
     }
+
+    setIsLoading(false);
   };
   useEffect(() => {
     refetch();
@@ -49,23 +50,26 @@ const useApiRequest = (method, url, data = null) => {
     setError(null);
 
     const apiClient = new ApiClient();
-
-    let response;
-    switch (tipo) {
-      case "get":
-        response = await apiClient.get(url);
-        break;
-      case "post":
-        response = await apiClient.post(url, data);
-        break;
-      case "put":
-        response = await apiClient.put(url, data);
-        break;
-      case "delete":
-        response = await apiClient.delete(url);
-        break;
-      default:
-        throw new Error(`Método HTTP inválido: ${tipo}`);
+    var response;
+    try {
+      switch (tipo) {
+        case "get":
+          response = await apiClient.get(url);
+          break;
+        case "post":
+          response = await apiClient.post(url, data);
+          break;
+        case "put":
+          response = await apiClient.put(url, data);
+          break;
+        case "delete":
+          response = await apiClient.delete(url);
+          break;
+        default:
+          throw new Error(`Método HTTP inválido: ${tipo}`);
+      }
+    } catch (err) {
+      console.log("deu erro");
     }
 
     setResponse(response);
