7e001673b01e153074694ff1ffbd2baea208796c
André Silva <andre06x@gmail.com>
2023-07-05 14:07:49 -0300

    adicionando componente tabela

--- src/becape-components/Pages/Historico/index.js
+++ src/becape-components/Pages/Historico/index.js
@@ -5,9 +5,38 @@ import { BsArrowLeft } from "react-icons/bs";
 
 import FalconCardHeader from "../../../components/common/FalconCardHeader";
 import { TabelaChamados } from "../../Components/TabelaChamados";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 const Historico = () => {
+  const [response, setResponse] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const contaId = Cookies.get("contaId");
+
   const history = useHistory();
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const dataPost = {
+          contaId
+        };
+        const response = await axios.get(
+          `http://165.22.184.175/conta-data?contaId=${contaId}&datainicio=0001-01-01&datafim=0001-01-01`,
+          dataPost
+        );
+        setResponse(response.data);
+        setIsLoading(false);
+      } catch (err) {
+        if (err.response.data) {
+          setResponse(err.response.data);
+        }
+        setIsLoading(false);
+      }
+    })();
+    // eslint-disable-next-line
+  }, []);
   return (
     <Card>
       <FalconCardHeader title={`Histórico`} titleClass="text-falar">
@@ -20,7 +49,11 @@ const Historico = () => {
         </button>
       </FalconCardHeader>
       <CardBody>
-        <TabelaChamados />
+        <TabelaChamados
+          dataChamadas={response}
+          setDataChamadas={setResponse}
+          isLoading={isLoading}
+        />
       </CardBody>
     </Card>
   );
